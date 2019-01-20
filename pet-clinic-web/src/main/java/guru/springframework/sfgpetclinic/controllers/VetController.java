7b5dd25ab860c7dd695b650edc871be8cb0070cc
Gerry Kaplan <gkaplan@kaplansoftware.com>
2019-01-19 23:15:36 -0500

    Added layout to vet page, closes #21

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,12 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -15,7 +18,8 @@ public class VetController {
 
     @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
-        model.addAttribute("vets",vetService.findAll());
+        Set<Vet> vets = vetService.findAll();
+        model.addAttribute("vets",vets);
 
         return "vets/index";
     }
